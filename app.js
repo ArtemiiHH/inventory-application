c3662d438a5e2b91f3276d4cd72cb4b4f833c6ac
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-11 15:24:18 +0200

    Set index route in app

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const indexRoute = require("./routes/index");
 
 // Set EJS as view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRoute);
 
 // Server
 const PORT = 3000;
