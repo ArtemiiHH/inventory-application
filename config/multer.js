581d540e7f3945470bef5bbfa4b1420cc9b1dc26
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-18 21:04:55 +0200

    Remove unnecessary lines in code

--- config/multer.js
+++ config/multer.js
@@ -1,14 +1,11 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-fs.mkdirSync("public/images", { recursive: true });
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/images/"); // saves to /public/images/
   },
   filename: function (req, file, cb) {
-    // e.g. "image-1710000000000.png" — unique name to avoid collisions
     const uniqueName = `image-${Date.now()}${path.extname(file.originalname)}`;
     cb(null, uniqueName);
   },
