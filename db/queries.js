ce5d03860da14cffe55892f5760dcf7483311f6b
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-21 15:53:02 +0200

    Add error handlers to all query funcs

--- db/queries.js
+++ db/queries.js
@@ -3,84 +3,114 @@ const pool = require("./pool");
 // Products
 // Get all products from DB
 exports.getAllProducts = async function () {
-  const { rows } = await pool.query("SELECT * FROM sneakers");
-  return rows;
+  try {
+    const { rows } = await pool.query("SELECT * FROM sneakers");
+    return rows;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 // Get product by id
 exports.getProductById = async function (id) {
-  const { rows } = await pool.query("SELECT * FROM sneakers WHERE id = $1", [
-    id,
-  ]);
+  try {
+    const { rows } = await pool.query("SELECT * FROM sneakers WHERE id = $1", [
+      id,
+    ]);
 
-  return rows[0]; // Return single product, not whole array
+    return rows[0]; // Return single product, not whole array
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 // Add new product to DB
 exports.addProductToDb = async function (newProduct) {
-  const image = newProduct.image_url
-    ? "/images/" + newProduct.image_url
-    : "/images/default-shoe.png";
+  try {
+    const image = newProduct.image_url
+      ? "/images/" + newProduct.image_url
+      : "/images/default-shoe.png";
 
-  await pool.query(
-    "INSERT INTO sneakers (name, brand, category, stock, price, description, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7)",
-    [
-      newProduct.name,
-      newProduct.brand,
-      newProduct.category,
-      newProduct.stock,
-      newProduct.price,
-      newProduct.description,
-      image,
-    ],
-  );
+    await pool.query(
+      "INSERT INTO sneakers (name, brand, category, stock, price, description, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+      [
+        newProduct.name,
+        newProduct.brand,
+        newProduct.category,
+        newProduct.stock,
+        newProduct.price,
+        newProduct.description,
+        image,
+      ],
+    );
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 // Delete product from DB
 exports.deleteProductFromDb = async function (id) {
-  await pool.query("DELETE FROM sneakers WHERE id = $1", [id]);
+  try {
+    await pool.query("DELETE FROM sneakers WHERE id = $1", [id]);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
 // Update product form
 exports.updateProduct = async function (id, updatedProduct) {
-  const image = updatedProduct.image_url
-    ? "/images/" + updatedProduct.image_url
-    : null;
+  try {
+    const image = updatedProduct.image_url
+      ? "/images/" + updatedProduct.image_url
+      : null;
 
-  if (image) {
-    await pool.query(
-      "UPDATE sneakers SET name = $1, brand = $2, category = $3, stock = $4, price = $5, description = $6, image_url = $7 WHERE id = $8",
-      [
-        updatedProduct.name,
-        updatedProduct.brand,
-        updatedProduct.category,
-        updatedProduct.stock,
-        updatedProduct.price,
-        updatedProduct.description,
-        image,
-        id,
-      ],
-    );
-  } else {
-    // No image update
-    await pool.query(
-      "UPDATE sneakers SET name = $1, brand = $2, category = $3, stock = $4, price = $5, description = $6 WHERE id = $7",
-      [
-        updatedProduct.name,
-        updatedProduct.brand,
-        updatedProduct.category,
-        updatedProduct.stock,
-        updatedProduct.price,
-        updatedProduct.description,
-        id,
-      ],
-    );
+    if (image) {
+      await pool.query(
+        "UPDATE sneakers SET name = $1, brand = $2, category = $3, stock = $4, price = $5, description = $6, image_url = $7 WHERE id = $8",
+        [
+          updatedProduct.name,
+          updatedProduct.brand,
+          updatedProduct.category,
+          updatedProduct.stock,
+          updatedProduct.price,
+          updatedProduct.description,
+          image,
+          id,
+        ],
+      );
+    } else {
+      // No image update
+      await pool.query(
+        "UPDATE sneakers SET name = $1, brand = $2, category = $3, stock = $4, price = $5, description = $6 WHERE id = $7",
+        [
+          updatedProduct.name,
+          updatedProduct.brand,
+          updatedProduct.category,
+          updatedProduct.stock,
+          updatedProduct.price,
+          updatedProduct.description,
+          id,
+        ],
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 };
 
 // Categories
 // Get all categories from DB
 exports.getAllCategories = async function () {
-  const { rows } = await pool.query("SELECT DISTINCT category FROM sneakers");
-  return rows;
+  try {
+    const { rows } = await pool.query("SELECT DISTINCT category FROM sneakers");
+    return rows;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
